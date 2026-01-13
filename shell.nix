850c789eb02387e45f77f5751322095329ad3331
Evie Litherland-Smith <evie.litherland-smith@ukaea.uk>
2026-01-13 10:46:57 +0000

    Set PV_PLUGIN_PATH in shell
    Adds imas-paraview plugins to path for paraview

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
 {
   pkgs ? import <nixpkgs> { },
+  python3 ? pkgs.python3,
   ...
 }:
 pkgs.mkShell {
-  packages = with pkgs; [
-    simdb
-    paraview
-    imas-paraview
+  env.PV_PLUGIN_PATH = "${python3.pkgs.imas-paraview}/lib/python${python3.pythonVersion}/site-packages/imas_paraview/plugins";
+  packages = [
+    pkgs.simdb
+    pkgs.paraview
+    (python3.withPackages (ps: [ ps.imas-paraview ]))
   ];
 }
