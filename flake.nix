10d0d8ae92b1f020d4cf9f183cd587c76d8a94ee
Evie Litherland-Smith <evie.litherland-smith@ukaea.uk>
2026-02-17 14:11:08 +0000

    Use pythonPackagesExtensions for overlay
    Remove mapping over specific python versions
    
    In devShell ensure same python version used for running paraview and
    getting the imas-paraview plugins

--- flake.nix
+++ flake.nix
@@ -7,37 +7,26 @@
     }:
     let
       system = "x86_64-linux";
-      supportedPythons = [
-        "python311"
-        "python312"
-        "python313"
-      ];
       pkgs = (nixpkgs.legacyPackages.${system}.extend self.overlays.default);
     in
     {
-      packages.${system} =
-        (builtins.listToAttrs (
-          builtins.concatMap (
-            python:
-            (builtins.map
-              (module: {
-                name = "${pkgs.${python}.libPrefix}-${module}";
-                value = pkgs.${python}.pkgs.${module};
-              })
-              [
-                "imas-core"
-                "imas-data-dictionaries"
-                "imas-python"
-                "imas-simdb"
-                "imas-paraview"
-              ]
-            )
-          ) supportedPythons
-        ))
-        // {
-          inherit (pkgs) simdb imas-paraview;
-          default = self.packages.${system}.simdb;
-        };
+      overlays.default = final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (pfinal: pprev: {
+            imas-core = pfinal.callPackage ./imas-core.nix { };
+            imas-data-dictionaries = pfinal.callPackage ./imas-data-dictionaries.nix { };
+            imas-python = pfinal.callPackage ./imas-python.nix { };
+            imas-simdb = pfinal.callPackage ./imas-simdb.nix { };
+            imas-paraview = pfinal.callPackage ./imas-paraview.nix { };
+          })
+        ];
+        simdb = final.python3Packages.toPythonApplication final.python3.pkgs.imas-simdb;
+        imas-paraview = final.python3Packages.toPythonApplication final.python3.pkgs.imas-paraview;
+      };
+      packages.${system} = {
+        inherit (pkgs) simdb imas-paraview;
+        default = self.packages.${system}.simdb;
+      };
       apps.${system} = {
         simdb = {
           type = "app";
@@ -46,33 +35,18 @@
         };
         default = self.apps.${system}.simdb;
       };
-      overlays.default =
-        final: prev:
-        {
-          simdb = final.python3Packages.toPythonApplication final.python3.pkgs.imas-simdb;
-          imas-paraview = final.python3Packages.toPythonApplication final.python3.pkgs.imas-paraview;
-        }
-        // (builtins.listToAttrs (
-          builtins.map (name: {
-            inherit name;
-            value = prev.${name}.override {
-              packageOverrides = pfinal: pprev: {
-                imas-core = pfinal.callPackage ./imas-core.nix { };
-                imas-data-dictionaries = pfinal.callPackage ./imas-data-dictionaries.nix { };
-                imas-python = pfinal.callPackage ./imas-python.nix { };
-                imas-simdb = pfinal.callPackage ./imas-simdb.nix { };
-                imas-paraview = pfinal.callPackage ./imas-paraview.nix { };
-              };
-            };
-          }) supportedPythons
-        ));
-      devShells.${system}.default = pkgs.mkShellNoCC {
-        env.PV_PLUGIN_PATH = "${pkgs.python3.pkgs.imas-paraview}/lib/python${pkgs.python3.pythonVersion}/site-packages/imas_paraview/plugins";
-        packages = with pkgs; [
-          simdb
-          imas-paraview
-          paraview
-        ];
-      };
+      devShells.${system}.default =
+        let
+          python3 = pkgs.python3;
+          imas-paraview = python3.pkgs.imas-paraview;
+        in
+        pkgs.mkShellNoCC {
+          env.PV_PLUGIN_PATH = "${imas-paraview}/lib/python${python3.pythonVersion}/site-packages/imas_paraview/plugins";
+          packages = [
+            pkgs.simdb
+            pkgs.paraview
+            (python3.pkgs.toPythonApplication imas-paraview)
+          ];
+        };
     };
 }
